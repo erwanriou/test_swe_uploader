984a6f1450616e6abe4927cbc6779d9251daeed0
erwanriou <rr.1.riou@gmail.com>
2026-01-18 14:44:49 +0100

    [Project] - fix a detail

---
+++ src/app.js
@@ -0,0 +1,35 @@
+require("express-async-errors")
+const express = require("express")
+const helmet = require("helmet")
+const compression = require("compression")
+const bodyParser = require("body-parser")
+const cookieSession = require("cookie-session")
+const cookieParser = require("cookie-parser")
+const { isError } = require("test_swe_common")
+
+// IMPORT ROUTES
+// TODO const routes = require("./routes")
+
+// LAUNCH EXPRESS
+const app = express()
+const secure = process.env.NODE_ENV !== "test"
+
+// USE MAIN MIDDELWWARE
+app.set("trust proxy", true)
+app.use(helmet())
+app.disable("x-powered-by")
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieSession({ name: "express:sess", signed: false, secure }))
+app.use(cookieParser())
+app.use(compression())
+
+// USE ROUTES
+app.get("/", (_, res) => res.status(200).send("Uploader"))
+app.get("/healthz.js", (_, res) => res.status(200).send("Healthz Check"))
+// TODO routes.map(route => app.use("/", route))
+
+// USE CUSTOM MIDDLWWARE
+app.use(isError)
+
+module.exports = app
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require("./app")
+// TODO const transaction = require("./services/transactions")
+
+// CONNECT DATABASE
+// TODO transaction("Uploader")
+
+// LISTEN APP
+app.listen(3000, () => console.log("Uploader listening on port 3000!"))
